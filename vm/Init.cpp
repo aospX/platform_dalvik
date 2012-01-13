3929147c786898438560fe7462349979efd75226
Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>
2012-01-13 14:44:44 +0059

    dalvik: Compile fixes with gcc 4.6
    Dalvik is compiled with -Werror, causing it to fail with gcc 4.6
    because it creates additional warnings - mostly about variables
    being defined but never used if LOG_NDEBUG is set.
    
    This fixes the warnings.
    
    Change-Id: If84d05a38b16ed3ef5eae2107ea40c614c5f57af
    Signed-off-by: Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>

--- vm/Init.cpp
+++ vm/Init.cpp
@@ -1146,6 +1146,12 @@ static void blockSignals()
         cc = sigaction(SIGBUS, &sa, NULL);
         assert(cc == 0);
     }
+#ifdef NDEBUG
+    // assert() is defined to nothing - resulting in
+    // cc: variable defined but not used (which breaks
+    // the build if -Werror is on)
+    (void)cc;
+#endif
 }
 
 class ScopedShutdown {
@@ -1448,10 +1454,12 @@ static bool initZygote()
  */
 bool dvmInitAfterZygote()
 {
+#ifndef LOG_NDEBUG
     u8 startHeap, startQuit, startJdwp;
     u8 endHeap, endQuit, endJdwp;
 
     startHeap = dvmGetRelativeTimeUsec();
+#endif
 
     /*
      * Post-zygote heap initialization, including starting
@@ -1460,8 +1468,10 @@ bool dvmInitAfterZygote()
     if (!dvmGcStartupAfterZygote())
         return false;
 
+#ifndef LOG_NDEBUG
     endHeap = dvmGetRelativeTimeUsec();
     startQuit = dvmGetRelativeTimeUsec();
+#endif
 
     /* start signal catcher thread that dumps stacks on SIGQUIT */
     if (!gDvm.reduceSignals && !gDvm.noQuitHandler) {
@@ -1475,8 +1485,10 @@ bool dvmInitAfterZygote()
             return false;
     }
 
+#ifndef LOG_NDEBUG
     endQuit = dvmGetRelativeTimeUsec();
     startJdwp = dvmGetRelativeTimeUsec();
+#endif
 
     /*
      * Start JDWP thread.  If the command-line debugger flags specified
@@ -1487,7 +1499,9 @@ bool dvmInitAfterZygote()
         LOGD("JDWP init failed; continuing anyway");
     }
 
+#ifndef LOG_NDEBUG
     endJdwp = dvmGetRelativeTimeUsec();
+#endif
 
     LOGV("thread-start heap=%d quit=%d jdwp=%d total=%d usec",
         (int)(endHeap-startHeap), (int)(endQuit-startQuit),
