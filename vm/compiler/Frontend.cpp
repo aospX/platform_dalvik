202ffb6ab422a083579cbdfbd5c37d545a38447d
Piyush Garyali <pgaryali@codeaurora.org>
2012-01-26 22:01:39 -0500

    vm: Add parameter to DEX verification method
    Add parameter to dvmVerifyDex method for future hooks
    
    (cherry picked from commit bd001a333392ad371725b33333033b491b1e321e)
    
    Change-Id: Ie98db935b9c6883b367941d792aa73055b7521f1

--- vm/compiler/Frontend.cpp
+++ vm/compiler/Frontend.cpp
@@ -1345,7 +1345,8 @@ bool dvmCompileMethod(const Method *method, JitTranslationInfo *info)
 /*
  * Utility funtion to check the DEX opcode for correctness
  */
-__attribute__((weak)) bool dvmVerifyDex(BasicBlock *curBlock, const u2* codePtr, MIR *insn)
+__attribute__((weak)) bool dvmVerifyDex(CompilationUnit *cUnit, BasicBlock *curBlock,
+                                        const u2* codePtr, MIR *insn)
 {
     bool result = false;
     if (insn) {
@@ -1394,7 +1395,7 @@ static bool exhaustTrace(CompilationUnit *cUnit, BasicBlock *curBlock)
         if (width == 0)
             break;
 
-        dvmVerifyDex(curBlock, codePtr + width, insn);
+        dvmVerifyDex(cUnit, curBlock, codePtr + width, insn);
         dvmCompilerAppendMIR(curBlock, insn);
 
         codePtr += width;
@@ -1782,7 +1783,7 @@ bool dvmCompileTrace(JitTraceDescription *desc, int numMaxInsts,
         assert(width);
         insn->width = width;
 
-        dvmVerifyDex(curBB, codePtr + width, insn);
+        dvmVerifyDex(&cUnit, curBB, codePtr + width, insn);
         traceSize += width;
         dvmCompilerAppendMIR(curBB, insn);
         cUnit.numInsts++;
